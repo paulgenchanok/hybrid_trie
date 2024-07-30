8001f7df22a5d4602c849d33b82950a7b1803c06
paul genchanok <pgenchanok@gmail.com>
2024-07-30 15:16:22 -0400

    remove commented code

--- src/main.c
+++ src/main.c
@@ -13,28 +13,6 @@
 int main(int argc, char ** argv)
 {
 
-    // trie_t * p_trie = create_trie();
-
-    // trie_load_wordlist(p_trie, "corncob_30000");
-    // trie_stat_print(p_trie);
-
-    // destroy_trie(p_trie);
-    // p_trie = NULL;
-
-    // printf("Normal Trie Stuff Finished\n");
-
-    // trie_csearch_t * p_ctrie = create_trie_csearch();
-
-    // // printf("About to load a wordlist\n");
-
-    // ctrie_load_wordlist(p_ctrie, "words_alpha.txt");
-    // // ctrie_stat_walker(p_ctrie->p_root, p_ctrie);
-    // // ctrie_stat_print(p_ctrie);
-    // ctrie_level_children_print(p_ctrie->p_root, 0);
-
-    // destroy_trie_csearch(p_ctrie);
-    // p_ctrie = NULL;
-
     htrie_t * p_htrie = htrie_create();
 
     htrie_load_wordlist(p_htrie, "words_alpha.txt");
@@ -42,7 +20,5 @@ int main(int argc, char ** argv)
     htrie_get_words(p_htrie, "abac", 4);
 
     htrie_destroy(p_htrie);
-
-    printf("destroyed?\n");
 
 }
