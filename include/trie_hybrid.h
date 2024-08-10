ec50cb0a850f46c89af85146b32ce5b60a4d3b7e
paul genchanok <pgenchanok@gmail.com>
2024-08-10 12:49:16 -0400

    reduce api

--- include/trie_hybrid.h
+++ include/trie_hybrid.h
@@ -26,27 +26,10 @@ typedef enum
 
 htrie_t * htrie_create();
 
-int8_t htrie_insert(htrie_t * p_htrie, char * p_word);
-
-void htrie_update_node_popularity(void * p_node, uint8_t depth);
-
 int8_t htrie_load_wordlist(htrie_t * p_htrie, char * p_wordlist_path);
 
 void htrie_destroy(htrie_t * p_htrie);
 
-int8_t htrie_get_words(htrie_t * p_htrie, char * p_prefix, uint8_t max_depth);
+int8_t htrie_put_letter(htrie_t * p_htrie, uint8_t letter);
 
-void * htrie_node_get_next(htrie_dnode_t * p_curr, \
-                    char      next,                 \
-                    char    * p_prefix_buff,        \
-                    uint8_t   prefix_len,           \
-                    uint8_t   max_depth);
-
-rec_array_t * htrie_fill_recs(void * p_curr, 
-                            char * p_prefix, 
-                            uint8_t prefix_len, 
-                            uint8_t max_depth);
-
-void htrie_render(rec_array_t * p_rat, htrie_render_action action);
-
-void htrie_autotyper(htrie_t * p_htrie);
+int8_t htrie_get_recs(rec_array_t * p_dst);
